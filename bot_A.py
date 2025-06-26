b96bc6f102f03871e55925a958e65aa6e9100ea0
thomfx <to.tobia@icloud.com>
2025-06-26 20:36:19 +0200

    Update bot_A.py

--- bot_A.py
+++ bot_A.py
@@ -1,23 +1,17 @@
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputFile
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7390879542:AAEdoyf9rgcgfdiWeSuPfK24Xd4HPZeG01g"
+TOKEN = "8117500006:AAEJTiGlh9So6xT-ESF5q54HBWKvHNlZE_E"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open("logo.jpg", "rb") as photo:
         await context.bot.send_photo(
             chat_id=update.effective_chat.id,
             photo=photo,
-            caption="Benvenuto nel bot VIP di Thom!\n\nAccedi subito a:\n- Le guide complete\n- Contatto diretto con me",
-
-Accedi subito a:
- Le guide complete
-  Contatto diretto con me",
-            parse_mode='Markdown',
+            caption="Benvenuto nel bot VIP di Thom!\n\nAccedi subito a:\n- Le guide complete\n- Contatto diretto con me.",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(" Scrivimi", url="https://t.me/ismaafx")],
-                [InlineKeyboardButton(" Vai alle Guide", url="https://t.me/ismaafx_guide_bot")]
+                [InlineKeyboardButton("Scrivimi", url="https://t.me/Thom_fx12")],
+                [InlineKeyboardButton("Vai alle Guide", url="https://t.me/Thom_guide_bot")]
             ])
         )
 
