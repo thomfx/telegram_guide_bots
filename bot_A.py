2f3ca4e5aadb1aafb81bc92fc6fa46b27d35c110
thomfx <to.tobia@icloud.com>
2025-06-26 22:15:53 +0200

    bot_A.py

--- bot_A.py
+++ bot_A.py
@@ -9,7 +9,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id=update.effective_chat.id,
     photo=photo,
     caption=caption,
-    parse_mode="HTLM"
+    parse_mode="HTML"
+)
             caption = """📈 <b>Benvenuto nel mio bot VIP!</b>
 
 Sono molto contento che da questo momento potrai approcciarti con noi nel mondo del trading, spero che tu possa trovarti bene in questo percorso.
